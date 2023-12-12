b890d381ec009c37d2ea1adf269011b4483eeae8
Tipusultan6y <tipusultan78600000@gmail.com>
2023-12-12 23:20:00 +0530

    initial

--- script.js
+++ script.js
@@ -252,10 +252,8 @@ function openNav(movie) {
           let {name, key, site} = video
 
           if(site == 'YouTube'){
-              
             embed.push(`
               <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          
           `)
 
             dots.push(`
@@ -392,3 +390,5 @@ function pageCall(page){
     getMovies(url);
   }
 }
+
+
